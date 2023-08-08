a634f3b87ef0e03be58fdde8af91c08974e19a1b
Pavlo Bilousov <pavlobil@amazon.com>
2023-08-08 18:04:31 +0200

    init cypress tests, add data-testid attributes to several UI elements

---
+++ cypress/e2e/fundamentals.cy.js
@@ -0,0 +1,17 @@
+describe('Fundamentals tests', () => {
+  beforeEach(() => {
+    cy.visit('/fundamentals')
+  })
+  
+  it('Contains correct header text', () => {
+    cy.getDataTest("fundamentals-header").should("contain.text", "Testing Fundamentals").contains(/Testing Fundamentals/i);
+  });
+
+  it('Accordion works correctly', () => {
+    cy.contains(/Your tests will exist in a describe block/i).should('not.be.visible');
+    cy.get('[data-testid="accordion-item-1"] div[role="button"]').click();
+    cy.contains(/Your tests will exist in a describe block/i).should('be.visible');
+    cy.get('[data-testid="accordion-item-1"] div[role="button"]').click();
+    cy.contains(/Your tests will exist in a describe block/i).should('not.be.visible');
+  });
+});
