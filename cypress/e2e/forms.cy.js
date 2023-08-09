e6c5b37099707d81117c80799daa8ef503c1e0ea
Pavlo Bilousov <bilousov.p@icloud.com>
2023-08-09 10:48:38 +0200

    add basic tests for forms page

---
+++ cypress/e2e/forms.cy.js
@@ -0,0 +1,13 @@
+describe('Form tests', () => {
+  beforeEach(() => {
+    cy.visit('/forms');
+  });
+
+  it('Test subscribe form', () => {
+    cy.contains(/testing forms/i);
+    cy.getDataTest('subscribe-form').find('input').type('ryan@coderyan.com');
+    cy.contains(/Successfully subbed: ryan@coderyan.com!/i).should("not.contain");
+    cy.getDataTest('subscribe-button').click();
+    cy.contains(/Successfully subbed: ryan@coderyan.com!/i).should("exist");
+  })
+});
